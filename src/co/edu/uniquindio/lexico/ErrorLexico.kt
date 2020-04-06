89d2236ac31c04192d0d07b33f08db8d3e204b09
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-04-05 20:59:57 -0500

    Clase Error Lexico
    * Se creo la clase Error Lexico

---
+++ src/co/edu/uniquindio/lexico/ErrorLexico.kt
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.lexico
+
+/**
+ * @author Samara Rincon
+ * @author Yesid Rosas Toro
+ * @author Cristian Camilo Quiceno
+ *
+ * @version 1.1
+ *
+ * Error Lexico
+ */
+class ErrorLexico(var palabra: String, var fila: Int, var columna: Int) {
+    override fun toString(): String {
+        return ("Error lexico '$palabra' en $fila:$columna")
+    }
+}
